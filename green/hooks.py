c07caa69d3ff1dbf4278d2ec2c12fa90844036b6
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-03-02 20:09:56 +0530

    Update hooks.py

--- green/hooks.py
+++ green/hooks.py
@@ -35,7 +35,9 @@ doctype_list_js = {"Item" : "public/js/item/item_list.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
 
 doctype_js = {"ToDo" : "public/js/todo.js",
-              "Stock Entry": "public/js/stock_entry.js"}
+              "Stock Entry": "public/js/stock_entry.js",
+             "Employee Checkin":"public/js/employee_checkin.js"
+             }
 
 # Home Pages
 # ----------
