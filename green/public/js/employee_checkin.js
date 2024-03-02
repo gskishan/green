9fd0b14ae9b4195eeba0a8750f7e3c7757b97e2f
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-03-02 20:20:05 +0530

    Update employee_checkin.js

--- green/public/js/employee_checkin.js
+++ green/public/js/employee_checkin.js
@@ -1,6 +1,6 @@
 frappe.ui.form.on("Employee Checkin", {
 refresh:function(frm){
-  console.log("checking refresh"
+  console.log("checking refresh")
                      },
 onload:function(frm){
   console.log("onload refresh"
