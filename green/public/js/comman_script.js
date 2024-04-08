d7634beb44d00861c15e5cd660ea619983e08626
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-04-08 18:42:35 +0530

    Update comman_script.js

--- green/public/js/comman_script.js
+++ green/public/js/comman_script.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
    setTimeout(function(){
     $('.search-bar').before('<div class="hello-text"><strong>' + frappe.defaults.get_user_default("Company") + '</strong></div>');
-   if (window.location.pathname === "/app/home") {
-    frappe.ui.toolbar.setup_session_defaults();
-  }  
+
    
    }, 1000);
    
