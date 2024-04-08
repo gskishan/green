449ddca2563cdb69b76008015e50befeaca721f6
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-04-08 17:32:52 +0530

    Create comman_script.js

---
+++ green/public/js/comman_script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+   setTimeout(function(){
+    $('.search-bar').before('<div class="hello-text"><strong>' + frappe.defaults.get_user_default("Company") + '</strong></div>');
+      }, 1000);
+});
