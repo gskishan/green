1e215073498a92632f9c1d619fdc0113d5e2bd4d
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-04-08 17:31:05 +0530

    Update hooks.py

--- green/hooks.py
+++ green/hooks.py
@@ -12,7 +12,7 @@ app_license = "MIT"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/green/css/green.css"
-# app_include_js = "/assets/green/js/green.js"
+app_include_js = "/assets/green/js/comman_script.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/green/css/green.css"
