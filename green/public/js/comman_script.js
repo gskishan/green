30e4c6460e4d04a88412016f15f36c630cfe595a
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-05-14 18:25:03 +0530

    Update comman_script.js

--- green/public/js/comman_script.js
+++ green/public/js/comman_script.js
@@ -7,28 +7,3 @@ $(document).ready(function() {
 	
  });
  
-
-function report_view() {
-    frappe.views.ReportView.prototype.get_count_element = function(df, data) {
-		let $count =0
-        return $count;
-    };
-}
-
-$(document).ready(function() {
-    report_view();
-
-    window.onpopstate = function() {
-        report_view();
-    };
-
-    $('.navbar').on('click', function(event) {
-        report_view();
-    });
-
-
-    $(document).on('click', function(event) {
-        report_view();
-    });
-
-})
