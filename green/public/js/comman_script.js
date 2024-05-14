796ebb1f9608029e49b06fe7f591020d16c4a7aa
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-05-14 17:51:38 +0530

    Update comman_script.js

--- green/public/js/comman_script.js
+++ green/public/js/comman_script.js
@@ -1,8 +1,34 @@
 $(document).ready(function() {
-   setTimeout(function(){
-    $('.search-bar').before('<div class="hello-text"><strong>' + frappe.defaults.get_user_default("Company") + '</strong></div>');
+	setTimeout(function(){
+	 $('.search-bar').before('<div class="hello-text"><strong>' + frappe.defaults.get_user_default("Company") + '</strong></div>');
+ 
+	
+	}, 1000);
+	
+ });
+ 
 
-   
-   }, 1000);
-   
-});
+function report_view() {
+    frappe.views.ReportView.prototype.get_count_element = function(df, data) {
+		let $count =0
+        return $count;
+    };
+}
+
+$(document).ready(function() {
+    report_view();
+
+    window.onpopstate = function() {
+        report_view();
+    };
+
+    $('.navbar').on('click', function(event) {
+        report_view();
+    });
+
+
+    $(document).on('click', function(event) {
+        report_view();
+    });
+
+})
