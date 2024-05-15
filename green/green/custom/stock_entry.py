b76de5a7cd7058896a5f74ff9bc63be8b743742c
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-05-15 12:49:26 +0530

    Update stock_entry.py

--- green/green/custom/stock_entry.py
+++ green/green/custom/stock_entry.py
@@ -15,7 +15,8 @@ def get_address_display(party):
 def add_list_view():
     sql="""select name from `tabDocType` where issingle=0 and istable=0"""
     for d in frappe.db.sql(sql,as_dict=True):
-        doc=frappe.new_doc("List View Settings")
-        doc.__newname=d.name
-        doc.disable_count=0
-        doc.save()
+        if d.name!="List View Settings":
+            doc=frappe.new_doc("List View Settings")
+            doc.__newname=d.name
+            doc.disable_count=0
+            doc.save()
