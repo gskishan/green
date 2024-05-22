585acfee2bad83062888327e879ea14551dd44cc
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-05-22 14:42:00 +0530

    Create event.py

---
+++ green/green/custom/event.py
@@ -0,0 +1,8 @@
+import frappe
+
+
+# from frappe.contacts.doctype.address.address import get_address_display
+
+@frappe.whitelist()
+def validate(self,method=None):
+    frappe.errprint([self.event_type,self.owner)]
