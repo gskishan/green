840e41fd9bf23dfa14ed21dee5176a97350a7395
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-06-03 16:42:54 +0530

    Update doc_name.py

--- green/green/custom/doc_name.py
+++ green/green/custom/doc_name.py
@@ -19,8 +19,7 @@ def set_name(doc):
     sql = """SELECT COUNT(name) AS seq FROM `tab{0}` WHERE YEAR({1}) = %s AND company = %s;""".format(doc.doctype, posting_dt)
     ct = frappe.db.sql(sql, (p_year, doc.company), as_dict=True)
     sequence = ct[0].seq + 1 if ct and ct[0].seq is not None else 1
-
+    
     sequence_formatted = '{:05d}'.format(sequence)
-
     doc.name = "{0}/{1}/{2}/{3}".format(doc_prefix, month, year, sequence_formatted)
     frappe.errprint(doc.name )
