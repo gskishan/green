bd3e06a6f419078fc93958c249b2c4230e1afcb9
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-06-07 20:43:05 +0530

    Create salary_paysheet.js

---
+++ green/green/report/salary_paysheet/salary_paysheet.js
@@ -0,0 +1,44 @@
+// Copyright (c) 2024, kushdhallod@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Salary Paysheet"] = {
+	"filters": [
+		{
+			"fieldname": "company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"width": "80",
+			"options": "Company",
+			"reqd": 1,
+			"default": frappe.defaults.get_default("company")
+		},
+		{
+			"fieldname": "from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"reqd": 1,
+			on_change: function() {
+				if (frappe.query_report.filters[1].value ){
+
+					frappe.query_report.set_filter_value('to_date', get_month_end(frappe.query_report.filters[1].value ));
+				}
+			}
+		},
+		{
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"reqd": 1,
+			
+
+		},
+
+	]
+};
+function get_month_end(date) {
+    var d = new Date(date);
+    return new Date(d.getFullYear(), d.getMonth() + 1, 0);
+}
+
