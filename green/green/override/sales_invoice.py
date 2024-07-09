368c77b3b58b5d1786bc4caef608761e0c837c7b
zafar <qutubuddinzafar2616118@gmail.com>
2024-07-09 17:51:28 +0530

    Sales Invoice Update Workflow When the Einvoice Closed

--- green/green/override/sales_invoice.py
+++ green/green/override/sales_invoice.py
@@ -7,3 +7,7 @@ from green.utils.naming import set_naming_counter
 class CustomSalesInvoice(SalesInvoice):
     def before_save(self):
         set_naming_counter(self)
+
+    def on_cancel(self):
+        self.workflow_state = "Cancelled"
+        self.db_update()
