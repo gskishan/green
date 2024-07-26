98a130b09d3e95d8fe24df2a1c0357641bc296a6
Mohammed  Qutubuddin Zafar <mohammedqutubuddinzafar@Mohammeds-MacBook-Air.local>
2024-07-26 16:49:09 +0530

    Make Change to Lead Status Field Old Docs of a Replied Lead

--- green/public/js/lead.js
+++ green/public/js/lead.js
@@ -15,7 +15,7 @@ frappe.ui.form.on('Lead', {
         }
     },
     custom_lead_status(frm){
-        let status_needed =[ 'Intake', 'Open', 'Inprogress', 'Qualified', 'Not Interested']
+        let status_needed =[ 'Intake', 'Open', 'Inprogress', 'Qualified', 'Not Interested', "Replied"]
         if(status_needed.includes(frm.doc.status)){
             frm.set_value("status", frm.doc.custom_lead_status)
             frm.refresh_field("status")
