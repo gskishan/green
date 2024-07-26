a1d729ec6dc02bdb2149738c608e55fa48057fd6
Mohammed  Qutubuddin Zafar <mohammedqutubuddinzafar@Mohammeds-MacBook-Air.local>
2024-07-26 18:15:21 +0530

    Removed Valid Till Empty

--- green/public/js/quotation.js
+++ green/public/js/quotation.js
@@ -5,11 +5,8 @@ frappe.ui.form.on("Quotation", {
 		if (frm.is_new()) {
 			frm.set_value(
 				"valid_till",
-				frappe.datetime.add_days(
-						frm.doc.transaction_date,
-						frappe.boot.sysdefaults.quotation_valid_till
-					)
-				)
+				""
+			)
 		}
 	}
 })
