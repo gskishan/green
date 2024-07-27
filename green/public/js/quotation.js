ae78c8545602d0d72874ed1d2cf2e22f14b84d50
Mohammed  Qutubuddin Zafar <mohammedqutubuddinzafar@Mohammeds-MacBook-Air.local>
2024-07-27 10:13:18 +0530

    Changed Event in Quotation for making Valid Till Empty

--- green/public/js/quotation.js
+++ green/public/js/quotation.js
@@ -1,12 +1,13 @@
 
 
 frappe.ui.form.on("Quotation", {
-	refresh: function (frm) {
-		if (frm.is_new()) {
+	onload_post_render: function (frm) {
+		if (frm.is_new() && frm.doc.__islocal) {
 			frm.set_value(
 				"valid_till",
 				""
 			)
+			frm.refresh_field("valid_till")
 		}
 	}
 })
