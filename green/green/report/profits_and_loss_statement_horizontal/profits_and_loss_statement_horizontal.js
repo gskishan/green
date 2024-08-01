ff0ff1210a3ff68296e37545eae1db72080b936b
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-08-01 20:32:50 +0530

    Update profits_and_loss_statement_horizontal.js

--- green/green/report/profits_and_loss_statement_horizontal/profits_and_loss_statement_horizontal.js
+++ green/green/report/profits_and_loss_statement_horizontal/profits_and_loss_statement_horizontal.js
@@ -1,114 +1,105 @@
-// Copyright (c) 2024, Anoop and contributors
-// For license information, please see license.txt
-/* eslint-disable */
-
 frappe.query_reports["Profits and Loss Statement Horizontal"] = {
 	"filters": [
         {
-            "fieldname": "company",
-            "label": __("Company"),
-            "fieldtype": "Link",
-            "options": "Company",
-            "default": "Quality Specialized Health Food Co. Ltd.",
-            "reqd": 1,
-            "placeholder": __("Company"),
-            "input_class": "input-xs"
-        },
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+			reqd: 1,
+		},
         {
-            "fieldname": "finance_book",
-            "label": __("Finance Book"),
-            "fieldtype": "Link",
-            "options": "Finance Book",
-            "placeholder": __("Finance Book"),
-            "input_class": "input-xs"
-        },
+			fieldname: "finance_book",
+			label: __("Finance Book"),
+			fieldtype: "Link",
+			options: "Finance Book",
+		},
+		{
+			fieldname: "filter_based_on",
+			label: __("Filter Based On"),
+			fieldtype: "Select",
+			options: ["Fiscal Year", "Date Range"],
+			default: "Fiscal Year",
+			reqd: 1,
+			on_change: function () {
+				let filter_based_on = frappe.query_report.get_filter_value("filter_based_on");
+				frappe.query_report.toggle_filter_display("from_fiscal_year", filter_based_on === "Fiscal Year");
+				frappe.query_report.toggle_filter_display("to_fiscal_year", filter_based_on === "Fiscal Year");
+				frappe.query_report.toggle_filter_display("period_start_date", filter_based_on === "Date Range");
+				frappe.query_report.toggle_filter_display("period_end_date", filter_based_on === "Date Range");
+
+				frappe.query_report.refresh();
+			},
+		},
         {
-            "fieldname": "filter_based_on",
-            "label": __("Filter Based On"),
-            "fieldtype": "Select",
-            "options": [
-                "Fiscal Year",
-                "Date Range"
-            ],
-            "default": "Fiscal Year",
-            "reqd": 1,
-            "placeholder": __("Filter Based On"),
-            "input_class": "input-xs"
-        },
+			fieldname: "period_start_date",
+			label: __("Start Date"),
+			fieldtype: "Date",
+			reqd: 1,
+			depends_on: "eval: frappe.query_report.get_filter_value('filter_based_on') == 'Date Range'",
+		},
+		{
+			fieldname: "period_end_date",
+			label: __("End Date"),
+			fieldtype: "Date",
+			reqd: 1,
+			depends_on: "eval: frappe.query_report.get_filter_value('filter_based_on') == 'Date Range'",
+		},
         {
-            "fieldname": "period_start_date",
-            "label": __("Start Date"),
-            "fieldtype": "Date",
-            "reqd": 1,
-            "depends_on": "eval:doc.filter_based_on == 'Date Range'",
-            "placeholder": __("Start Date"),
-            "input_class": "input-xs",
-            "hidden_due_to_dependency": true
-        },
+			fieldname: "periodicity",
+			label: __("Periodicity"),
+			fieldtype: "Select",
+			options: [
+				{ value: "Monthly", label: __("Monthly") },
+				{ value: "Quarterly", label: __("Quarterly") },
+				{ value: "Half-Yearly", label: __("Half-Yearly") },
+				{ value: "Yearly", label: __("Yearly") },
+			],
+			default: "Yearly",
+			reqd: 1,
+		},
         {
-            "fieldname": "period_end_date",
-            "label": __("End Date"),
-            "fieldtype": "Date",
-            "reqd": 1,
-            "depends_on": "eval:doc.filter_based_on == 'Date Range'",
-            "placeholder": __("End Date"),
-            "input_class": "input-xs",
-            "hidden_due_to_dependency": true
-        },
+			fieldname: "from_fiscal_year",
+			label: __("Start Year"),
+			fieldtype: "Link",
+			options: "Fiscal Year",
+			reqd: 1,
+			depends_on: "eval: frappe.query_report.get_filter_value('filter_based_on') == 'Fiscal Year'",
+		},
+		{
+			fieldname: "to_fiscal_year",
+			label: __("End Year"),
+			fieldtype: "Link",
+			options: "Fiscal Year",
+			reqd: 1,
+			depends_on: "eval: frappe.query_report.get_filter_value('filter_based_on') == 'Fiscal Year'",
+		},
+		{
+			fieldname: "presentation_currency",
+			label: __("Currency"),
+			fieldtype: "Select",
+			options: erpnext.get_presentation_currency_list(),
+		},
         {
-            "fieldname": "from_fiscal_year",
-            "label": __("Start Year"),
-            "fieldtype": "Link",
-            "options": "Fiscal Year",
-            "default": "2023",
-            "reqd": 1,
-            "depends_on": "eval:doc.filter_based_on == 'Fiscal Year'",
-            "placeholder": __("Start Year"),
-            "input_class": "input-xs"
-        },
-        {
-            "fieldname": "to_fiscal_year",
-            "label": __("End Year"),
-            "fieldtype": "Link",
-            "options": "Fiscal Year",
-            "default": "2023",
-            "reqd": 1,
-            "depends_on": "eval:doc.filter_based_on == 'Fiscal Year'",
-            "placeholder": __("End Year"),
-            "input_class": "input-xs"
-        },
-        {
-            "fieldname": "periodicity",
-            "label": __("Periodicity"),
-            "fieldtype": "Select",
-            "options": [
-                { "value": "Monthly", "label": __("Monthly") },
-                { "value": "Quarterly", "label": __("Quarterly") },
-                { "value": "Half-Yearly", "label": __("Half-Yearly") },
-                { "value": "Yearly", "label": __("Yearly") }
-            ],
-            "default": "Yearly",
-            "reqd": 1,
-            "placeholder": __("Periodicity"),
-            "input_class": "input-xs"
-        },
-        {
-            "fieldname": "presentation_currency",
-            "label": __("Currency"),
-            "fieldtype": "Select",
-            "options": [
-                "", "AED", "AUD", "CHF", "CNY", "EUR", "GBP", "INR", "JPY", "SAR", "USD"
-            ],
-            "placeholder": __("Currency"),
-            "input_class": "input-xs"
-        },
-        {
-            "fieldname": "cost_center",
-            "label": __("Cost Center"),
-            "fieldtype": "MultiSelectList",
-            "placeholder": __("Cost Center"),
-            "input_class": "input-xs"
-        }
+			fieldname: "cost_center",
+			label: __("Cost Center"),
+			fieldtype: "MultiSelectList",
+			get_data: function (txt) {
+				return frappe.db.get_link_options("Cost Center", txt, {
+					company: frappe.query_report.get_filter_value("company"),
+				});
+			},
+		},
+		{
+			fieldname: "project",
+			label: __("Project"),
+			fieldtype: "MultiSelectList",
+			get_data: function (txt) {
+				return frappe.db.get_link_options("Project", txt, {
+					company: frappe.query_report.get_filter_value("company"),
+				});
+			},
+		},
     ],
 
     // "onload": function(report) {
@@ -132,5 +123,3 @@ frappe.query_reports["Profits and Loss Statement Horizontal"] = {
     //     });
     // }
 };
-
-
