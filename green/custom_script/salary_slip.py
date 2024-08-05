8257c876364f4dade7cb6eb330fdba64c2423750
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-08-05 20:46:56 +0530

    Update salary_slip.py

--- green/custom_script/salary_slip.py
+++ green/custom_script/salary_slip.py
@@ -25,9 +25,6 @@ from frappe.utils import (
 
 class CustomSalarySlip(SalarySlip):
 
-
-
-
 	@frappe.whitelist()
 	def pull_sal_struct(self):
 		from hrms.payroll.doctype.salary_structure.salary_structure import make_salary_slip
@@ -64,106 +61,26 @@ class CustomSalarySlip(SalarySlip):
 
 		self.get_late_record()
 
-	def get_working_days_details(self, lwp=None, for_preview=0):
-		payroll_settings = frappe.get_cached_value(
-			"Payroll Settings",
-			None,
-			(
-				"payroll_based_on",
-				"include_holidays_in_total_working_days",
-				"consider_marked_attendance_on_holidays",
-				"daily_wages_fraction_for_half_day",
-				"consider_unmarked_attendance_as",
-			),
-			as_dict=1,
-		)
 
-		consider_marked_attendance_on_holidays = (
-			payroll_settings.include_holidays_in_total_working_days
-			and payroll_settings.consider_marked_attendance_on_holidays
-		)
-
-		daily_wages_fraction_for_half_day = flt(payroll_settings.daily_wages_fraction_for_half_day) or 0.5
-
-		working_days = date_diff(self.end_date, self.start_date) + 1
-		if for_preview:
-			self.total_working_days = working_days
-			self.payment_days = working_days
-			return
-
-		holidays = self.get_holidays_for_employee(self.start_date, self.end_date)
-		working_days_list = [add_days(getdate(self.start_date), days=day) for day in range(0, working_days)]
-
-		if not cint(payroll_settings.include_holidays_in_total_working_days):
-			working_days_list = [i for i in working_days_list if i not in holidays]
-
-			working_days -= len(holidays)
-			if working_days < 0:
-				frappe.throw(_("There are more holidays than working days this month."))
-
-		if not payroll_settings.payroll_based_on:
-			frappe.throw(_("Please set Payroll based on in Payroll settings"))
-
-		if payroll_settings.payroll_based_on == "Attendance":
-			actual_lwp, absent = self.calculate_lwp_ppl_and_absent_days_based_on_attendance(
-				holidays, daily_wages_fraction_for_half_day, consider_marked_attendance_on_holidays
-			)
-			self.absent_days = absent
-		else:
-			actual_lwp = self.calculate_lwp_or_ppl_based_on_leave_application(
-				holidays, working_days_list, daily_wages_fraction_for_half_day
-			)
-
-		if not lwp:
-			lwp = actual_lwp
-		elif lwp != actual_lwp:
-			frappe.msgprint(
-				_("Leave Without Pay does not match with approved {} records").format(
-					payroll_settings.payroll_based_on
-				)
-			)
-
-		self.leave_without_pay = lwp
-		self.total_working_days = working_days
-
-		payment_days = self.get_payment_days(payroll_settings.include_holidays_in_total_working_days)
-
-		if flt(payment_days) > flt(lwp):
-			self.payment_days = flt(payment_days) - flt(lwp)
-
-			if payroll_settings.payroll_based_on == "Attendance":
-				self.payment_days -= flt(absent)
-
-			consider_unmarked_attendance_as = payroll_settings.consider_unmarked_attendance_as or "Present"
-
-			if (
-				payroll_settings.payroll_based_on == "Attendance"
-				and consider_unmarked_attendance_as == "Absent"
-			):
-				unmarked_days = self.get_unmarked_days(
-					payroll_settings.include_holidays_in_total_working_days, holidays
-				)
-				self.absent_days += unmarked_days  # will be treated as absent
-				self.payment_days -= unmarked_days
-		else:
-			self.payment_days = 0
+	def before_validate(self):
 		self.payment_days = (self.payment_days or 0) - (self.absent_days or 0)
 		
 	@frappe.whitelist()
 	def get_late_record(self):
-		date= self.start_date.split("-")
-		filters={
-			'month': date[1], 
-			'year': date[0], 
-			'company': self.company, 
-			'summarized_view': 1
-			}
+		if self.start_date:
+			date= self.start_date.split("-")
+			filters={
+				'month': date[1], 
+				'year': date[0], 
+				'company': self.company, 
+				'summarized_view': 1
+				}
 
-		data=get_late_entries(self.employee,filters)
-		self.set("custom_late_entry_days",data.total_late_entries)
-		self.update_payment_days()
-		self.set('custom_late_leave_days', (self.custom_late_entry_days // 3))
-		self.add_late_deduction()
+			data=get_late_entries(self.employee,filters)
+			self.set("custom_late_entry_days",data.total_late_entries)
+			self.update_payment_days()
+			self.set('custom_late_leave_days', (self.custom_late_entry_days // 3))
+			self.add_late_deduction()
 
 
 	@frappe.whitelist()
