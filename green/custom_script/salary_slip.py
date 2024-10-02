5bbebf8a6f62fafea2ba8509f47249870e956d09
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-10-02 17:56:58 +0530

    Update salary_slip.py

--- green/custom_script/salary_slip.py
+++ green/custom_script/salary_slip.py
@@ -3,7 +3,7 @@ from frappe import _
 from hrms.payroll.doctype.salary_slip.salary_slip import SalarySlip
 from hrms.payroll.doctype.salary_slip.salary_slip import *
 from frappe.utils import flt
-from hrms.hr.utils import  validate_active_employee
+from hrms.hr.utils import validate_active_employee
 from hrms.hr.report.monthly_attendance_sheet.monthly_attendance_sheet import get_entry_exits_summary
 import json
 from frappe.utils import (
@@ -22,29 +22,20 @@ from frappe.utils import (
 	rounded,
 )
 
-
 class CustomSalarySlip(SalarySlip):
 
 	@frappe.whitelist()
 	def pull_sal_struct(self):
 		from hrms.payroll.doctype.salary_structure.salary_structure import make_salary_slip
-		rt=0
+		rt = 0
 		if self.salary_slip_based_on_timesheet:
 			self.salary_structure = self._salary_structure_doc.name
 			self.total_working_hours = sum([d.working_hours or 0.0 for d in self.timesheets]) or 0.0
 			make_salary_slip(self._salary_structure_doc.name, self)
-			# deduct=0
-			# for d in self.deductions:
-			# 	if d.salary_component=='Provident Fund Employee' or  d.salary_component=='ESIC Employer':
-			# 		deduct+=d.amount
-			# adding=0
-			# for e in self.earnings:
-			# 	if e.salary_component == 'Basic' or e.salary_component == 'Conveyance Allowance' or e.salary_component == 'House Rent Allowance' or e.salary_component == 'Medical Allowance':
-			# 		adding += e.amount
-			
+
 			self.set("earnings", [])
 			self.set("deductions", [])
-			base=get_base_amount(self.employee)
+			base = get_base_amount(self.employee)
 			frappe.msgprint(_("Base amount retrieved: {0}").format(base))
 			rt = ((base / self.total_working_days) / 8.0)
 			frappe.msgprint(_("Base amount retrieved: {0}").format(rt))
@@ -61,81 +52,69 @@ class CustomSalarySlip(SalarySlip):
 
 		self.get_late_record()
 
-		
 	@frappe.whitelist()
 	def get_late_record(self):
 		if self.start_date:
 			if isinstance(self.start_date, str):
-            			date = self.start_date.split("-")
-        		else:
-           		 	date = self.start_date.strftime("%Y-%m-%d").split("-")
+				date = self.start_date.split("-")
+			else:
+				date = self.start_date.strftime("%Y-%m-%d").split("-")
 			filters = {
-				'month': date[1], 
-				'year': date[0], 
-				'company': self.company, 
+				'month': date[1],
+				'year': date[0],
+				'company': self.company,
 				'summarized_view': 1
 			}
 
 			data = get_late_entries(self.employee, filters)
-			self.set("custom_late_entry_days", data.total_late_entries) 
-			
+			self.set("custom_late_entry_days", data.total_late_entries)
+
 			# Calculate late leave days based on total late entries without any deduction
 			self.set('custom_late_leave_days', (self.custom_late_entry_days // 3))
 			self.update_payment_days()
 			self.add_late_deduction()
+
 	def after_insert(self):
 		self.save()
+
 	@frappe.whitelist()
 	def add_late_deduction(self):
-		if self.custom_late_leave_days>0:
-			base=get_base_amount(self.employee)
+		if self.custom_late_leave_days > 0:
+			base = get_base_amount(self.employee)
 			rt = ((base / self.total_working_days) * self.custom_late_leave_days)
-			update=True
+			update = True
 			for s in self.deductions:
-				if s.salary_component=="Late Attendance":
-					s.amount=rt
-					update=False
-					
+				if s.salary_component == "Late Attendance":
+					s.amount = rt
+					update = False
 
-					
-			if update:	
-				deduct=self.append("deductions",{})
-				deduct.salary_component="Late Attendance"
-				deduct.amount=rt
-		#self.payment_days = (self.payment_days or 0) - (self.absent_days or 0)
-
-
-
-
+			if update:
+				deduct = self.append("deductions", {})
+				deduct.salary_component = "Late Attendance"
+				deduct.amount = rt
 
 	@frappe.whitelist()
 	def update_payment_days(self):
 		payment_days = self.get('payment_days')
 		custom_late_entry_days = self.get('custom_late_entry_days')
-	
+
 		if payment_days and payment_days > 1 and custom_late_entry_days >= 3:
 			new_payment_days = payment_days - (custom_late_entry_days // 3)
 			self.set('payment_days', new_payment_days)
-		
-		
 
 def get_base_amount(employee):
-	sql="""select base from `tabSalary Structure Assignment` where employee="{0}" """.format(employee)
-	base=frappe.db.sql(sql,as_dict=True)
+	sql = """select base from `tabSalary Structure Assignment` where employee="{0}" """.format(employee)
+	base = frappe.db.sql(sql, as_dict=True)
 	if base:
 		return base[0].base
 	else:
-		frappe.msgprint("issue in finding salary assigment")
+		frappe.msgprint("issue in finding salary assignment")
 		return 0
-
-
-
-
 
 @frappe.whitelist()
 def get_late_entries(employee, filters):
-    if isinstance(filters, str):
-        filters = json.loads(filters)
-    if not isinstance(filters, frappe._dict):
-        filters = frappe._dict(filters)
-    return get_entry_exits_summary(employee, filters)
+	if isinstance(filters, str):
+		filters = json.loads(filters)
+	if not isinstance(filters, frappe._dict):
+		filters = frappe._dict(filters)
+	return get_entry_exits_summary(employee, filters)
