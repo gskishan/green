d09d246c2cf6ac2df6a7433023bdf33013d4e5b7
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-10-26 21:01:27 +0530

    Update task_list.js

--- green/custom_script/task/task_list.js
+++ green/custom_script/task/task_list.js
@@ -1,14 +1,14 @@
-frappe.listview_settings['Task'] = {
-    refresh: function (listview) {
-        listview.page.add_inner_button(__("In PDF"), () => {
-            const ganttChartSVG = document.querySelector('.gantt');
+// frappe.listview_settings['Task'] = {
+//     refresh: function (listview) {
+//         listview.page.add_inner_button(__("In PDF"), () => {
+//             const ganttChartSVG = document.querySelector('.gantt');
             
-            if (ganttChartSVG) {
-                svgExport.downloadPdf(
-                    ganttChartSVG,
-                    "Task Export"
-                );
-            }
-        }, __("Export Gantt"))
-    }
-}
+//             if (ganttChartSVG) {
+//                 svgExport.downloadPdf(
+//                     ganttChartSVG,
+//                     "Task Export"
+//                 );
+//             }
+//         }, __("Export Gantt"))
+//     }
+// }
